2873a666ed663a8fe22c1ba67da2760a3b0f3587
Andii Karamushko <karamushko1997@gmail.com>
2025-11-02 18:56:17 +0200

    ref: add cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,5 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  // Твой существующий плагин i18n
   i18n: {
     enabled: true,
     config: {
@@ -6,4 +7,20 @@ module.exports = {
       locales: ["uk", "en"],
     },
   },
-};
+
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
