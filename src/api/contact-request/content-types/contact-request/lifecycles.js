ad483ab36a90d13d5a7892cd2ef9c959b2d097cf
Andii Karamushko <karamushko1997@gmail.com>
2025-11-25 22:41:26 +0200

    ref: add lifecycles.js

---
+++ src/api/contact-request/content-types/contact-request/lifecycles.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async afterCreate(event) {
+    const { result } = event;
+
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "karamushko1997@gmail.com",
+        from: "onboarding@resend.dev",
+        subject: `Новая заявка: ${result.name} ${result.surname}`,
+        text: `
+          Имя: ${result.name}
+          Фамилия: ${result.surname}
+          Телефон: ${result.tel}
+          Email: ${result.email}
+          Сообщение: ${result.message}
+        `,
+        html: `
+          <h3>Новая заявка с сайта! 🐶</h3>
+          <p><b>Имя:</b> ${result.name}</p>
+          <p><b>Фамилия:</b> ${result.surname}</p>
+          <p><b>Телефон:</b> <a href="tel:${result.tel}">${result.tel}</a></p>
+          <p><b>Email:</b> ${result.email}</p>
+          <p><b>Сообщение:</b><br/> ${result.message}</p>
+        `,
+      });
+
+      console.log("Email sent successfully!");
+    } catch (err) {
+      console.error("Failed to send email:", err);
+    }
+  },
+};
